3177502f52eea3cc660ecf26fb90f46f59ef19bf
Gwanjung Kim <96829017+kimgwanjung@users.noreply.github.com>
2022-03-30 22:09:58 +0900

    Flip char

---
+++ CharFlip.java
@@ -0,0 +1,30 @@
+package Practice;
+import java.util.*;
+public class CharFlip {
+	public String solution(String str){
+	String answer;
+	char[] s=str.toCharArray();
+	int lt=0, rt=str.length()-1;
+	while(lt<rt){
+		if(!Character.isAlphabetic(s[lt])) lt++;
+		else if(!Character.isAlphabetic(s[rt])) rt--;
+		else{
+			char tmp=s[lt];
+			s[lt]=s[rt];
+			s[rt]=tmp;
+			lt++;
+			rt--;
+		}
+	}
+	answer=String.valueOf(s);
+	return answer;
+}
+
+public static void main(String[] args){
+	CharFlip T = new CharFlip();
+	Scanner kb = new Scanner(System.in);
+	String str=kb.next();
+	System.out.println(T.solution(str));
+}
+
+}
